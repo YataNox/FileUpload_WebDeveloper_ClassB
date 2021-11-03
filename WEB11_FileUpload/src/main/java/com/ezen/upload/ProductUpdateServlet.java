f8d6241aaeb9131f3dca5e1211aa4e657842c88f
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-03 18:06:57 +0900

    Create & Write ProductUpdateServlet - doGet Method

---
+++ WEB11_FileUpload/src/main/java/com/ezen/upload/ProductUpdateServlet.java
@@ -0,0 +1,52 @@
+package com.ezen.upload;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ezen.dao.ProductDao;
+import com.ezen.dto.ProductVO;
+
+/**
+ * Servlet implementation class ProductUpdateServlet
+ */
+@WebServlet("/update.do")
+public class ProductUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductUpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String code = request.getParameter("code");
+		
+		ProductDao pdao = ProductDao.getInstance();
+		ProductVO pvo = pdao.getProduct(code);
+		
+		request.setAttribute("product", pvo);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("product/productUpdate.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
