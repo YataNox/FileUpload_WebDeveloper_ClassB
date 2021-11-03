1efbd940006d3b09857ac14361d049b1b660c338
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-03 17:25:45 +0900

    Write doPost Method

--- WEB11_FileUpload/src/main/java/com/ezen/upload/ProductWriteServlet.java
+++ WEB11_FileUpload/src/main/java/com/ezen/upload/ProductWriteServlet.java
@@ -3,11 +3,17 @@ package com.ezen.upload;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ezen.dao.ProductDao;
+import com.ezen.dto.ProductVO;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 /**
  * Servlet implementation class ProductWriteServlet
@@ -36,7 +42,30 @@ public class ProductWriteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		ServletContext context = getServletContext();
+		String path = context.getRealPath("fileUpload");
+		
+		ProductVO pvo = new ProductVO();
+		
+		try {
+			MultipartRequest multi = new MultipartRequest(request,
+					path, 5*1024*1024, "UTF-8",
+					new DefaultFileRenamePolicy());
+			
+			pvo.setName(multi.getParameter("name"));
+			pvo.setPrice(Integer.parseInt(multi.getParameter("price")));
+			pvo.setDescription(multi.getParameter("description"));
+			pvo.setPictureurl(multi.getParameter("pictureurl"));
+		}catch(Exception e) {
+			System.out.println("파일 업로드 실패 : " + e);
+		}		
+		
+		ProductDao pdao = ProductDao.getInstance();
+		pdao.insertProduct(pvo);
+		// 목록으로 되돌아 갑니다.
+		response.sendRedirect("productlist.do");
 	}
 
 }
